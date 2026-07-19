f6e83922f4911fe842face551a87732be3f01569
haunh766 <nguyenhuuhau@MacBook-of-Huu-Hau.local>
2026-07-19 14:05:31 +0700

    buoi 20

--- obj/Debug/net10.0/buoi18.AssemblyInfo.cs
+++ obj/Debug/net10.0/buoi18.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("buoi18")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29f35e92bd4bd5756321a9f86711277a7a8b88af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c24a13397b9b4d2937ab13695db335e3a55feba9")]
 [assembly: System.Reflection.AssemblyProductAttribute("buoi18")]
 [assembly: System.Reflection.AssemblyTitleAttribute("buoi18")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
